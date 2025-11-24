ebbc026f52347dc8e6b2b4e62f0701f61a2a7a1a
Adonis Cesar Legón Campo <alegon@gmail.com>
2025-11-24 14:54:53 -0300

    Bump version to 0.2.2

--- derpy/__init__.py
+++ derpy/__init__.py
@@ -5,6 +5,6 @@ A Python CLI application that provides essential container functionality
 without relying on existing container runtimes like Docker, Podman, or containerd.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Adonis Cesar Legón Campo"
 __email__ = "alegon@gmail.com"
